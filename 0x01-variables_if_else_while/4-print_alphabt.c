0170d87487455c9841d483708b2addeb73c18dc8
benjaminogwezi@gmail.com <benjaminogwezi@gmail.com>
2023-06-19 00:19:28 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,12 +2,15 @@
 /**
  *main-program entry point.
  *Return:0 if no error, non zero if error.
- **/
+ */
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
+		if (i == 'e' || i == 'q')
+			continue;
 		putchar(i);
 	}
 	putchar('\n');
